5a0bce46b7311ae7e5d7a87ef98c9fcdb7dba2a0
Aastha Neupane <aastha.pane12@gmail.com>
2021-06-21 15:25:39 -0400

    updated flattened

--- arrays/faltten.js
+++ arrays/faltten.js
@@ -1,9 +1,6 @@
 // Flatten a given array, eliminating nested and empty arrays. Do not alter the array; return a new one retaining the order. 
 // Example #1) [1, [2,3], 4, []] ==> return a new array [1,2,3,4]
 
-// Slightly harder: Work in-place in the given array (do not create a new array). Alter the order if needed! 
-// Example #2) [1, [2,3], 4, []] ==> return same array but adjusted [1,2,3,4]
-
 // **Assume that all elements are either numbers or arrays
 
 function flatten(arr) {
