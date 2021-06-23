b125c57fe2225b4145cff791c9112bbd1aa7dc9b
Aastha Neupane <aastha.pane12@gmail.com>
2021-06-23 17:22:50 -0400

    Combine sort added

--- arrays/sort/combineSort.js
+++ arrays/sort/combineSort.js
@@ -1,9 +1,8 @@
-//Combine arrays
+// Combine arrays
+// ---------------
 // Given two arrays that are sorted, combine them into one sorted array
 // Ex: 
-// var sort1 = [2,4,6,8,10];
-// var sort2 = [1,3,9,11,13];
-// Should return -> [1,2,3,4,6,8,9,10,11,13]
+// var sort1 = [2,4,6,8,10]; var sort2 = [1,3,9,11,13]; Should return -> [1,2,3,4,6,8,9,10,11,13]
 
 function combineArrays(arr, arr2){
 
