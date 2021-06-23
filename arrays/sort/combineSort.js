67485352982c2240bd45e2b7b91c0fc1c35d6d53
Aastha Neupane <aastha.pane12@gmail.com>
2021-06-23 15:09:47 -0400

    Combine sort added

---
+++ arrays/sort/combineSort.js
@@ -0,0 +1,26 @@
+//Combine arrays
+// Given two arrays that are sorted, combine them into one sorted array
+// Ex: 
+// var sort1 = [2,4,6,8,10];
+// var sort2 = [1,3,9,11,13];
+// Should return -> [1,2,3,4,6,8,9,10,11,13]
+
+function combineArrays(arr, arr2){
+
+    var sortArr = [];
+    var i = 0;
+    var j = 0;
+    while(sortArr.length<(arr.length+arr2.length)){
+        if(arr[i]<arr2[j]){
+            sortArr.push(arr[i]);
+            i++;
+        }
+        else{
+            sortArr.push(arr2[j]);
+            j++
+        }
+    }
+    return sortArr;
+}
+
+console.log(combineArrays([2,4,6,8,10], [1,3,9,11,13]))
