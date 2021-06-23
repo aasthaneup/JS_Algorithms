dd169acb1522efa828d3f1a18660021def815286
Aastha Neupane <aastha.pane12@gmail.com>
2021-06-23 17:58:29 -0400

    Partition sort added

--- arrays/sort/partitionSort.js
+++ arrays/sort/partitionSort.js
@@ -1,4 +1,5 @@
-//Array Partition
+// Array Partition
+// ---------------
 // Given an array of unsorted numbers and a pivot index, sort the array so that all values less than the value at the pivot index are on the left of the value and all values greater than the pivot value are on the right
 // Ex: 
 // Given var sortArr = [7,9,1,6,2,10,8,4,3] and a pivot index of 3, return [1,2,4,3,6,9,7,10,8]
@@ -85,10 +86,11 @@ function partitionSort(arr, pivot){
     return arr;
 }
 
+console.log("=================Method 1=================")
 console.log(partition([7,9,1,6,2,10,8,4,3], 3));
 console.log("==================================")
 console.log(partition([3,2,7,4,9,1,6], 3))
-console.log("==================================")
+console.log("================Method 2==================")
 console.log(partitionSort([7,9,1,6,2,10,8,4,3], 3));
 console.log("==================================")
 console.log(partitionSort([3,2,7,4,9,1,6], 3))
