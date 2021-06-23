fa469ebf59355ffda318539893630f4cf19bf9aa
Aastha Neupane <aastha.pane12@gmail.com>
2021-06-23 17:59:50 -0400

    Partition sort updated

--- arrays/sort/partitionSort.js
+++ arrays/sort/partitionSort.js
@@ -5,7 +5,6 @@
 // Given var sortArr = [7,9,1,6,2,10,8,4,3] and a pivot index of 3, return [1,2,4,3,6,9,7,10,8]
 // Notice how the pivot index of 3 lands on the value of 6 originally and then sorts all numbers in the array around being less than or greater than the number 6
 // It is OKAY for the pivot to move, just keep track of the original VALUE of the pivot
-// Starter code:
 
 function partition(arr, pivot){
     for(let i = 0;i<arr.length;i++){
