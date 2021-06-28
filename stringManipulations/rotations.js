63e8f3b0e4723f6b01b4c0cef5b2f18cc6aa6217
Aastha Neupane <aastha.pane12@gmail.com>
2021-06-28 16:15:24 -0400

    isRotation? added

--- stringManipulations/rotations.js
+++ stringManipulations/rotations.js
@@ -25,6 +25,40 @@ const rotateString = (str, num) => {
     return str;
 }
 
+//  Is Rotation
+// Conversely, given a string and a possibly rotated string, return true or false whether the second string is a rotation of the first
+// Ex: given isRotation("Hello World", "rldHello Wo"), return true
+// Ex: given isRotation("Hello", "World"), return false
+
+const isRotation = (str1, str2) => {
+    var rotn = false;
+    if(str1.length != str2.length){
+        console.log("Is not a rotation because the lengths of the two strings is not equal!")
+        return false;
+    }
+    for(let i = 1; i<=str1.length;i++){
+        if(!rotn){
+            let rotatedStr = rotateString(str1, i)
+            rotatedStr == str2 ? rotn = true : rotn = false;
+            // console.log("rotated str is: "+rotatedStr);
+            // console.log("rot is: "+rotn);
+        }
+        else{
+            return true;
+        }
+    }
+    return rotn;
+}
+
+console.log("===========Rotate the String=============")
 console.log(rotateString("Hello World", 3));
-console.log("========================")
+console.log("----------------------------")
 console.log(rotateString("Rotated", 4));
+console.log("============Is Rotation?============")
+console.log(isRotation("Hello World", "rldHello Wo"));
+console.log("----------------------------")
+console.log(isRotation("Hello", "World"));
+console.log("----------------------------")
+console.log(isRotation("Hello", "loHel"));
+console.log("----------------------------")
+console.log(isRotation("Hello", "loH"));
