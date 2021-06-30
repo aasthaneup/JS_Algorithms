12705c4506b8fd3c30a291a309a0afeb717d2071
Aastha Neupane <aastha.pane12@gmail.com>
2021-06-30 17:16:18 -0400

    matrix search updated

--- arrays/matrixSearch.js
+++ arrays/matrixSearch.js
@@ -1,10 +1,15 @@
 // Matrix Search
 // ==============
+
 // Jenny digs image and wants to make a JavaScript imaging library like PIL for python. She is given 2 different two-dimensional arrays, containing integers between 0 and 65535. Each two dimensional array represents a grey-scale image, where each integer value is a pixel. The second image might be somewhere inside the larger one. Return whether it is.
+
 // Given Array: [ [12, 34, 45, 56],    And Array: [ [67, 78],      return: true
 //                [98, 87, 76, 65],                 [43, 32] ];   
 //                [56, 67, 78, 89],
 //                [54, 43, 32, 21] ];
+
+// Solution:
+// =========
 
 function matrixSearch(array, subArray) {
     for(let i=0; i< subArray.length; i++){
